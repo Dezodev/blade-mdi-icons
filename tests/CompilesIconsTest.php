c7290318993a7a22fc01900f481af1ed28770cf3
Dezodev <Dezodev@users.noreply.github.com>
2024-05-28 08:09:15 +0000

    Fix code styling

--- tests/CompilesIconsTest.php
+++ tests/CompilesIconsTest.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace Tests;
 
-use BladeUI\MDIIcons\BladeMDIIconsServiceProvider;
 use BladeUI\Icons\BladeIconsServiceProvider;
+use BladeUI\MDIIcons\BladeMDIIconsServiceProvider;
 use Orchestra\Testbench\TestCase;
 
 class CompilesIconsTest extends TestCase
